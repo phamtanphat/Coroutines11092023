3a3ab7a103703e17a5ba6a03584dbfe6823052af
phamtanphat <phatdroid94@gmail.com>
2023-12-12 20:41:50 +0700

    launch va witch context

--- app/src/main/java/com/example/coroutines11092023/MainActivity.kt
+++ app/src/main/java/com/example/coroutines11092023/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.coroutines11092023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineName
+import android.util.Log
+import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Kotlin la ngôn ngữ hỗ trợ xử lý đa luồng vì khi trình biên dịch vẫn đang sử dụng java
+        // Có thể tham khảo sử dụng tạo luồng bằng Thread hoặc Coroutine
 
         // Coroutine context: Chua cac element de cau hinh cho 1 coroutine
         // 1-Job: Quyet dinh vong doi cua 1 coroutine
@@ -22,9 +27,27 @@ class MainActivity : AppCompatActivity() {
         // 4-CoroutineName: Dat ten cho coroutine
 
         // Coroutine scope: Tao ra pham vi de su dung coroutine
-
         // Dispatchers.IO: luong background
-        CoroutineScope( CoroutineName("A") + Dispatchers.IO + CoroutineExceptionHandler() + Job())
+        CoroutineScope(Dispatchers.IO).launch {
+           launch {
+               for (i in 1..100) {
+                   Log.d("pphat", "A $i")
+               }
 
+               withContext(Dispatchers.Main) {
+                   Toast.makeText(this@MainActivity, "Finish A", Toast.LENGTH_SHORT).show()
+               }
+           }
+
+           launch {
+               for (i in 101..200) {
+                   Log.d("pphat", "B $i")
+               }
+
+               withContext(Dispatchers.Main) {
+                   Toast.makeText(this@MainActivity, "Finish B", Toast.LENGTH_SHORT).show()
+               }
+           }
+        }
     }
 }
